f40c67430e8bfd1d7064b96879eb35f2c7333fb2
Christoffer Hasselberg <stofolus@gmail.com>
2015-12-19 23:02:31 +0100

    Added support for min, max & step

--- slider.js
+++ slider.js
@@ -4,7 +4,12 @@ angular.module('chasselberg.slider', [])
 function SliderDirective() {
     var directive = {
         restrict: 'EA',
-        scope: {},
+        scope: {
+            model: '=',
+            min: '=',
+            max: '=',
+            step: '='
+        },
         link: linkFunction,
         template: '<div class="ch-slider">' +
             '<div class="ch-slider-bar">' +
@@ -18,6 +23,9 @@ function SliderDirective() {
     function linkFunction($scope, $element) {
         var $bar = angular.element($element[0].querySelector('.ch-slider-bar'));
         var $handle = angular.element($element[0].querySelector('.ch-slider-handle'));
+        $scope.min = ($scope.min === undefined) ? 0 : $scope.min;
+        $scope.max = ($scope.max === undefined) ? 100 : $scope.max;
+        $scope.step = ($scope.step === undefined) ? 1 : $scope.step;
 
         handleWatcher();
 
@@ -34,7 +42,7 @@ function SliderDirective() {
             });
             angular.element(window).on('mousemove', function(event) {
                 if (isActive) {
-                    console.log('MouseMove!');
+                    event.preventDefault();
                     calcPosition(event);
                 }
             });
@@ -45,18 +53,47 @@ function SliderDirective() {
                 barWidth = $bar[0].getBoundingClientRect().width,
                 barOffset = (($bar[0].getBoundingClientRect().left + document.body.scrollLeft) - event.pageX),
                 handlePosition = Math.abs(barOffset);
-                if(handlePosition < 0 || barOffset > 0) {
-                    handlePosition = 0;
-                }
-                if(handlePosition > barWidth) {
-                    handlePosition = barWidth;
-                }
-                console.log(barOffset);
-                console.log(handlePosition);
-                handlePosition = handlePosition - halfOfHandle;
-                console.log(handlePosition);
-                $handle.css('left', handlePosition + 'px');
 
+            if (barOffset > 0) {
+                handlePosition = $scope.min;
+            }
+
+            var offsetPercentage = percent(handlePosition, barWidth),
+                value = ($scope.max - $scope.min) * offsetPercentage;
+
+            setValue(value);
+
+        }
+
+        function setValue(value) {
+
+            value = roundToInterval(value, $scope.step);
+
+            if (value < $scope.min) {
+                value = $scope.min;
+            } else if (value > $scope.max) {
+                value = $scope.max;
+            }
+
+            var halfOfHandle = $handle[0].getBoundingClientRect().width / 2,
+                percentage = percent(value, $scope.max - $scope.min) * 100;
+
+            $handle.css('left', 'calc(' + percentage + '% - ' + halfOfHandle + 'px)');
+            $scope.model = value;
+            $scope.$apply();
+        }
+
+        function percent(frac, num) {
+            return (frac / num);
+        }
+
+        function roundToInterval(number, interval) {
+            if (number > 0)
+                return Math.ceil(number / interval) * interval;
+            else if (number < 0)
+                return Math.floor(number / interval) * interval;
+            else
+                return number;
         }
     };
 
