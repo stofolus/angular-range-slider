c8f06f0755ab094c5483a774ad70975f25cccf92
Christoffer Hasselberg <stofolus@gmail.com>
2015-12-20 10:54:04 +0100

    Aria attributes and better min, max, step

--- slider.js
+++ slider.js
@@ -13,7 +13,13 @@ function SliderDirective() {
         link: linkFunction,
         template: '<div class="ch-slider">' +
             '<div class="ch-slider-bar">' +
-            '<span class="ch-slider-handle"></span>' +
+            '<span class="ch-slider-handle" ' +
+            'role="slider" ' +
+            'aria-valuemin="{{ min }}" ' +
+            'aria-valuemax="{{ max }}" ' +
+            'aria-valuenow="{{ model }}" ' +
+            'aria-orientation="horizontal" ' +
+            '></span>' +
             '<span class="ch-slider-fill></span>"' +
             '</div>' +
             '</div>'
@@ -23,30 +29,64 @@ function SliderDirective() {
     function linkFunction($scope, $element) {
         var $bar = angular.element($element[0].querySelector('.ch-slider-bar'));
         var $handle = angular.element($element[0].querySelector('.ch-slider-handle'));
-        $scope.min = ($scope.min === undefined) ? 0 : $scope.min;
-        $scope.max = ($scope.max === undefined) ? 100 : $scope.max;
-        $scope.step = ($scope.step === undefined) ? 1 : $scope.step;
 
-        setupEvents();
+        init();
+
+        function init() {
+            $scope.min = ($scope.min === undefined) ? 0 : $scope.min;
+            $scope.max = ($scope.max === undefined) ? 100 : $scope.max;
+            $scope.step = ($scope.step === undefined) ? 1 : $scope.step;
+            $scope.model = ($scope.model === undefined) ? ($scope.max - $scope.min) / 2 : $scope.model;
+
+            setupEvents();
+            setupWatches();
+
+        }
 
 
         function setupEvents() {
             var isActive = false;
             $handle.on('mousedown', function() {
                 isActive = true;
-                angular.element(window).on('mousemove', moveFunction);
+                angular.element(window).on('mousemove', move);
+                angular.element(window).on('mouseup', end);
             });
-            angular.element(window).on('mouseup', function() {
-                if (isActive) {
-                    isActive = false;
-                    angular.element(window).off('mousemove', moveFunction);
-                }
-            });
-            function moveFunction(event) {
+
+            function move(event) {
                 if (isActive) {
                     event.preventDefault();
                     calcPosition(event);
                 }
+            }
+
+            function end(event) {
+                if (isActive) {
+                    isActive = false;
+                    angular.element(window).off('mousemove', move);
+                    angular.element(window).off('mouseup', end);
+                }
+            }
+        }
+
+        function setupWatches() {
+            $scope.$watch(function() {
+                return $scope.model;
+            }, function(value) {
+                setValue(value, false);
+            });
+
+            $scope.$watch(function() {
+                return $scope.min;
+            }, updateEverything);
+            $scope.$watch(function() {
+                return $scope.max;
+            }, updateEverything);
+            $scope.$watch(function() {
+                return $scope.step;
+            }, updateEverything);
+
+            function updateEverything() {
+                setValue($scope.model);
             }
         }
 
@@ -58,18 +98,24 @@ function SliderDirective() {
 
             if (barOffset > 0) {
                 handlePosition = $scope.min;
+                value = handlePosition;
+            } else {
+                var offsetPercentage = percent(handlePosition, barWidth),
+                    value = (($scope.max - $scope.min) * offsetPercentage) + parseFloat($scope.min);
             }
 
-            var offsetPercentage = percent(handlePosition, barWidth),
-                value = ($scope.max - $scope.min) * offsetPercentage;
-
-            setValue(value);
+            console.log(value);
+            setValue(value, true, true);
 
         }
 
-        function setValue(value) {
+        function setValue(value, normalize, forceDigest) {
+            normalize = (normalize === undefined) ? true : normalize;
+            forceDigest = (forceDigest === undefined) ? false : forceDigest;
 
-            value = roundToInterval(value, $scope.step);
+            if (normalize) {
+                value = roundToInterval(value, $scope.step);
+            }
 
             if (value < $scope.min) {
                 value = $scope.min;
@@ -78,11 +124,21 @@ function SliderDirective() {
             }
 
             var halfOfHandle = $handle[0].getBoundingClientRect().width / 2,
-                percentage = percent(value, $scope.max - $scope.min) * 100;
+                percentage = percent(value - $scope.min, $scope.max - $scope.min) * 100;
+
+            if(percentage < 0) {
+                percentage = 0;
+            } else if(percentage > 100) {
+                percentage = 100;
+            }
 
             $handle.css('left', 'calc(' + percentage + '% - ' + halfOfHandle + 'px)');
-            $scope.model = value;
-            $scope.$apply();
+            if (normalize) {
+                $scope.model = value;
+                if (forceDigest) {
+                    $scope.$apply();
+                }
+            }
         }
 
         function percent(frac, num) {
